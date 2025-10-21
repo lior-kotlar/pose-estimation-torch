54980b989fd5dfd5c20a1448ed48231871a16803
lior-kotlar <lior.kotlar@gmail.com>
2025-10-21 12:48:09 +0300

    removed all lines referring to multigpu training

--- code/training_code/train.py
+++ code/training_code/train.py
@@ -3,7 +3,6 @@ import sys
 abspath = os.path.abspath(__file__)
 code_directory = os.path.dirname(os.path.dirname(abspath))
 sys.path.append(code_directory)
-import random
 import torch
 from datetime import date
 import time
@@ -14,22 +13,13 @@ import numpy as np
 import torch.optim.lr_scheduler as lr_scheduler
 from utils import *
 import Callbacks
-# import torch.multiprocessing as mp
-# from torch.nn.parallel import DistributedDataParallel as DDP
-# from torch.distributed import init_process_group, destroy_process_group, reduce, ReduceOp, get_world_size
+
 
 N = 0
 C = 1
 H = 2
 W = 3
 REPORT_EVERY = 100
-
-# def ddp_setup(rank, world_size, port, use_gpu):
-#     os.environ["MASTER_ADDR"] = "localhost"
-#     # os.environ["MASTER_PORT"] = "12355"
-#     os.environ["MASTER_PORT"] = port
-#     backend = 'nccl' if use_gpu else "gloo"
-#     init_process_group(backend=backend, rank=rank, world_size=world_size)
 
 def arrange_loaded_checkpoint(general_configuration: Config):
     '''
@@ -51,7 +41,6 @@ class Trainer:
     def __init__(self,
                  general_configuration: Config,
                  base_run_directory,
-                 gpu_id,
                  device):
         self.device = device
         if general_configuration.debug_mode:
@@ -59,7 +48,6 @@ class Trainer:
 
         self.base_run_directory = base_run_directory
         self.val_fraction = general_configuration.get_val_fraction()
-        self.gpu_id = gpu_id
         self.preprocessor = Preprocessor.Preprocessor(general_configuration)
         self.best_val_loss = float("inf")
         self.start_epoch = 0
@@ -83,10 +71,6 @@ class Trainer:
                                        number_of_output_channels=self.num_output_channels)
         self.model = self.network.get_model()
         self.model.to(self.device)
-        # if self.device.type == 'cuda':
-        #     self.model = DDP(self.model, device_ids=[self.gpu_id])
-        # else:
-        #     self.model = DDP(self.model)
 
         self.loss_function = loss_from_string[general_configuration.loss_function_as_string]()
         self.optimizer = optimizer_from_string[general_configuration.optimizer_as_string](
@@ -110,11 +94,10 @@ class Trainer:
         self.train_box, self.train_confmap, self.val_box, self.val_confmap, _, _ = self.train_val_split()
         self.validation = (self.val_box, self.val_confmap)
         self.viz_sample = (self.val_box[general_configuration.viz_idx], self.val_confmap[general_configuration.viz_idx])
-        if self.gpu_id == 0:
-            print("img_size:", self.img_size, flush=True)
-            print("num_output_channels:", self.num_output_channels, flush=True)
-            show_sample_channels(self.viz_sample[0], self.base_run_directory, filename="viz_sample.png")
-            show_interest_points_with_index(self.viz_sample[0], self.viz_sample[1], self.base_run_directory, filename="viz_sample_points.png")
+        print("img_size:", self.img_size, flush=True)
+        print("num_output_channels:", self.num_output_channels, flush=True)
+        show_sample_channels(self.viz_sample[0], self.base_run_directory, filename="viz_sample.png")
+        show_interest_points_with_index(self.viz_sample[0], self.viz_sample[1], self.base_run_directory, filename="viz_sample_points.png")
         # test_transforms(self.train_box[0], self.train_confmap[0], self.base_run_directory, [
         #     Augmentor.Scale()
         # ])
@@ -168,8 +151,7 @@ class Trainer:
         return loss.item()
 
     def do_one_epoch(self, epoch_number, train_loader, val_loader):
-        if self.gpu_id == 0:
-            self.callbacks.on_epoch_begin(epoch=epoch_number)
+        self.callbacks.on_epoch_begin(epoch=epoch_number)
 
         whole_epoch_train_loss_sum = 0.0
         step_count = 0
@@ -187,13 +169,8 @@ class Trainer:
             step_count += 1
 
         avg_train_loss = whole_epoch_train_loss_sum / step_count
-        # avg_train_loss_tensor = torch.tensor([avg_train_loss], device=self.device)
-        # reduce(avg_train_loss_tensor, dst=0, op=ReduceOp.SUM)
-        # if self.gpu_id == 0:
-        #     general_avg_train_loss = avg_train_loss_tensor / get_world_size()
-        #     logs['train loss'] = general_avg_train_loss.item()
         logs['train loss'] = avg_train_loss
-            
+        
         running_val_loss = 0.0
         step_count = 0
         self.model.eval()
@@ -211,22 +188,14 @@ class Trainer:
 
         if average_val_loss < self.best_val_loss:
             self.best_val_loss = average_val_loss
-            if self.gpu_id == 0:
-                self._save_checkpoint(epoch=epoch_number, best=True)
+            self._save_checkpoint(epoch=epoch_number, best=True)
 
-        # average_val_loss_tensor = torch.tensor([average_val_loss], device=self.device)
-        # reduce(average_val_loss_tensor, dst=0, op=ReduceOp.SUM)
-        # if self.gpu_id == 0:
-        #     general_avg_validation_loss = average_val_loss_tensor / get_world_size()
-        #     # print(f'[ALL GPUs] | Epoch {epoch_number}/{self.num_epochs} | Train Loss: {general_avg_train_loss.item():.4f}, Validation Loss: {general_avg_validation_loss.item():.8f}')
-        #     logs['validation loss'] = general_avg_validation_loss.item()
         logs['validation loss'] = average_val_loss
 
         self.lr_scheduler.step(average_val_loss)
         logs['lr'] = self.lr_scheduler.get_last_lr()[0]
 
-        if self.gpu_id == 0:
-            self.callbacks.on_epoch_end(epoch=epoch_number, logs=logs)
+        self.callbacks.on_epoch_end(epoch=epoch_number, logs=logs)
 
     def train(self):
         augmentor = Datasets.Augmentor(self.general_configuration)
@@ -236,14 +205,11 @@ class Trainer:
         val_loader = Datasets.prepare_dataloader(val_set, self.general_configuration.batch_size)
 
         training_start_time = time.time()
-        if self.gpu_id == 0:
-            self.callbacks.on_train_start()
+        self.callbacks.on_train_start()
 
         for epoch in range(self.start_epoch, self.num_epochs):
-            # train_loader.sampler.set_epoch(epoch)
             self.do_one_epoch(epoch_number=epoch, train_loader=train_loader, val_loader=val_loader)
-            if self.gpu_id == 0 and \
-                self.general_configuration.save_every > 0 and \
+            if self.general_configuration.save_every > 0 and \
                     epoch % self.general_configuration.save_every == 0:
                 self._save_checkpoint(epoch=epoch)
 
@@ -252,8 +218,7 @@ class Trainer:
         elapsed_time = training_end_time - training_start_time
         hours, rem = divmod(elapsed_time, 3600)
         minutes, seconds = divmod(rem, 60)
-        if self.gpu_id == 0:
-            print(f'Training completed in {int(hours):0>2}:{int(minutes):0>2}:{int(seconds):0>2} (hh:mm:ss)', flush=True)
+        print(f'Training completed in {int(hours):0>2}:{int(minutes):0>2}:{int(seconds):0>2} (hh:mm:ss)', flush=True)
 
         
     def train_val_split(self, shuffle=True):
@@ -266,33 +231,28 @@ class Trainer:
         idx = idx[val_size:]
         return self.box[idx], self.confmaps[idx], self.box[val_idx], self.confmaps[val_idx], idx, val_idx
 
-def joint_main(rank,
-               world_size,
-               general_configuration,
-               base_run_directory,
-               port,
-               use_gpu):
-    # ddp_setup(rank=rank, world_size=world_size, port=port, use_gpu=use_gpu)
+def training_main(
+                general_configuration,
+                base_run_directory,
+                use_gpu
+               ):
     if use_gpu:
-        device = torch.device(f'cuda:{rank}')
-        print(f"[Rank {rank}] Running on {torch.cuda.get_device_name(device=device)}", flush=True)
+        device = torch.device(f'cuda:{0}')
+        print(f"Running on {torch.cuda.get_device_name(device=device)}", flush=True)
     else:
         device = torch.device("cpu")
-        print(f"[Rank {rank}] Running on CPU", flush=True)
+        print(f"Running on CPU", flush=True)
     try:
         trainer = Trainer(
             general_configuration=general_configuration,
             base_run_directory=base_run_directory,
-            gpu_id=rank,
             device=device
         )
         trainer.train()        
     except Exception as e:
         import traceback
-        print(f"[Rank {rank}] Exception during training: {e}", flush=True)
+        print(f"Exception during training: {e}", flush=True)
         traceback.print_exc()
-    # finally:
-    #     destroy_process_group()
 
 def main():
     config_path = sys.argv[1] if len(sys.argv) > 1 else exit("Please provide a config file.")
@@ -311,28 +271,15 @@ def main():
     
     if torch.cuda.is_available():
         print(f"Using GPU: {torch.cuda.get_device_name(0)}", flush=True)
-        # world_size = torch.cuda.device_count()
-        # current_learning_rate = general_configuration.get_learning_rate()
-        # general_configuration.set_learning_rate(current_learning_rate * world_size)
         use_gpu = True
     else:
-        # world_size = 1
         use_gpu = False
         print("⚠️ GPU not available, using CPU instead.", flush=True)
 
-    # port = str(random.randint(10000, 20000))  # pick free port
-    # mp.spawn(joint_main, args=(world_size,
-    #                            general_configuration,
-    #                            base_output_directory,
-    #                            port,
-    #                            use_gpu), nprocs=world_size)
 
-    joint_main(
-        rank=0,
-        world_size=-1,
+    training_main(
         general_configuration=general_configuration,
         base_run_directory=base_output_directory,
-        port="12355",
         use_gpu=use_gpu
     )
     
